c2eff588ceeabba9fd8884111494303373bcf15e
SilentSupernova <chrisyoules93@gmail.com>
2017-06-19 16:13:56 +0100

    All the whiteboard

---
+++ Whiteboard/Assets/Pen.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pen : MonoBehaviour {
+
+    public Whiteboard _whiteboard;
+    private RaycastHit touch;
+    public Transform _tip;
+    public float drawThresHold;
+    private bool lastTouch;
+    private Quaternion lastAngle;
+    public Color penColor;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Vector3 tipPos = _tip.transform.position;
+
+        if(Physics.Raycast(tipPos, transform.up ,out touch, drawThresHold))
+        {
+            if (touch.collider.tag == "Whiteboard")
+            {
+               // Debug.Log("Poop");
+                this._whiteboard = touch.collider.gameObject.GetComponent<Whiteboard>();
+
+                _whiteboard.SetColour(penColor);
+                _whiteboard.SetTouchPosition(touch.textureCoord.x, touch.textureCoord.y);
+                _whiteboard.ToggleTouch(true);
+
+
+
+                if (!lastTouch)
+                {
+                    lastTouch = true;
+                    lastAngle = this.transform.rotation;
+                }
+            }
+            else
+            {
+                _whiteboard.ToggleTouch(false);
+                lastTouch = false;
+            }
+        }
+        if (lastTouch)
+        {
+            transform.rotation = lastAngle;
+        }
+	}
+}
---
+++ Whiteboard/Assets/Whiteboard.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Whiteboard : MonoBehaviour
+{
+
+    private int textureSize = 2048;
+    private int pensize = 10;
+    private Texture2D texture;
+    private Color[] colours;
+
+
+    private bool touching, touchingLast;
+
+    float posX, posY, lastX, lastY;
+
+    // Use this for initialization
+    void Start()
+    {
+        Renderer rend = GetComponent<Renderer>();
+        texture = new Texture2D(textureSize, textureSize);
+        rend.material.mainTexture = texture;
+    }
+
+    void Update()
+    {
+        int x = (int)(posX * textureSize - (pensize / 2));
+        int y = (int)(posY * textureSize - (pensize / 2));
+
+        if(touchingLast)
+        {
+            texture.SetPixels(x, y, pensize, pensize, colours);
+
+            for (float t = 0.01f; t < 1.00f; t += 0.01f)
+            {
+                int lerpX = (int)Mathf.Lerp(lastX, (float)x, t);
+                int lerpY = (int)Mathf.Lerp(lastY, (float)y, t);
+                texture.SetPixels(lerpX, lerpY, pensize, pensize, colours);
+            }
+
+            texture.Apply();
+        }
+
+        lastX = (float)x;
+        lastY = (float)y;
+
+        touchingLast = touching;
+    }
+
+
+    public void ToggleTouch(bool touchingBoard)
+    {
+        this.touching = touchingBoard;
+    }
+
+    public void SetTouchPosition(float x, float y)
+    {
+        posX = x;
+        posY = y;
+    }
+
+    public void SetColour(Color color)
+    {
+        colours = Enumerable.Repeat<Color>(color, pensize * pensize).ToArray<Color>();
+    }
+}
